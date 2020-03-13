5117ff64271c3f7b3eacdd853f490cac3d405824
Saurav <sauravsrivastav103@gmail.com>
2020-03-13 23:58:46 +0530

    Commenting the code

--- Map1.py
+++ Map1.py
@@ -1,10 +1,10 @@
 import folium
 import pandas
 
-data = pandas.read_csv("Volcanoes.txt")
-lat = list(data["LAT"])
-lon = list(data["LON"])
-elev = list(data["ELEV"])
+data = pandas.read_csv("Volcanoes.txt")     #Reading the csv file
+lat = list(data["LAT"])                     #stored the data in the "LAT" column of the data frame to a list
+lon = list(data["LON"])                     #stored the data in the "LON" column of the data frame to a list
+elev = list(data["ELEV"])                   #stored the data in the "ELEV" column of the data frame to a list
 
 def color_producer(elevation):
     if elevation < 1000 :
@@ -14,9 +14,9 @@ def color_producer(elevation):
     else :
         return 'red'
 
-map = folium.Map(location=[35.8543,-121.3712], zoom_start=6)
+map = folium.Map(location=[35.8543,-121.3712], zoom_start=6)        #creates an object of the Map class and opens at the given location
 
-fg=folium.FeatureGroup(name="My Map")
+fg=folium.FeatureGroup(name="My Map")       #making a FeatureGroup class object which will be added as a child to the base Map.
 
 for lt, ln, el in zip(lat, lon, elev) :
     fg.add_child(folium.CircleMarker(location=[lt, ln],
@@ -27,6 +27,6 @@ for lt, ln, el in zip(lat, lon, elev) :
                                      color = 'grey',
                                      fill_opacity = 0.7))
 
-map.add_child(fg)
+map.add_child(fg)                 #Adds the feature group layer to the Map.
 
-map.save("Map1.html")
+map.save("Map1.html")           #Saves the Map
