604c929e0a2e94159b605d945473d274f8cb82d7
Saurav <sauravsrivastav103@gmail.com>
2020-03-17 00:51:27 +0530

    Color based polygon features depending on the population of each country, basically a choropleth Map

--- Map1.py
+++ Map1.py
@@ -14,7 +14,7 @@ def color_producer(elevation):              #We produce the color of the marker 
     else :
         return 'red'
 
-map = folium.Map(location=[35.8543,-121.3712], zoom_start=6)        #creates an object of the Map class and opens at the given location
+map = folium.Map(location=[35.8543,-121.3712], zoom_start=6, tiles="Mapbox Bright")        #creates an object of the Map class and opens at the given location
 
 fg=folium.FeatureGroup(name="My Map")       #making a FeatureGroup class object which will be added as a child to the base Map.
 
@@ -27,7 +27,12 @@ for lt, ln, el in zip(lat, lon, elev) :
                                      color = 'grey',
                                      fill_opacity = 0.7))
 
-fg.add_child(folium.GeoJson(data = (open('world.json', 'r', encoding = 'utf-8-'))))     #creates a GeoJson object for plotting into a map.
+fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(),
+                            style_function = lambda x: {'fillColor': 'yellow' if x['properties']['POP2005']<10000000
+                            else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000
+                            else 'red'}))
+#The lambda function has been used to go through the POP2005 attribute of the Json file and depending on it's value the color is decided.
+#creates a GeoJson object for plotting into a map.
 # In this case we mark the borders of different countries by uusig the data in the Json file.
 #Adds a GeoJson polygon layer onto the Map.
 
